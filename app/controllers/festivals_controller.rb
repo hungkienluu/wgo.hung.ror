0d6aeb3cd12c9286b9d19036476615b878352f28
Hung Luu <hungkienluu@gmail.com>
2012-08-15 05:14:21 +0000

    changed to remove offsetfrom festival

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,21 +1,21 @@
 class FestivalsController < ApplicationController
 
-  # GET /users
-  # GET /users.json
+  # GET /festivals
+  # GET /festivals.json
   # http://wgo-hung-ror.herokuapp.com/users/insertUser//changed to blooming something heroku
   def index
-    @offset, @limit = api_offset_and_limit
-    @festivals =  Festival.find :all,
-                        :limit  =>  @limit,
-                        :offset =>  @offset
+    # @offset, @limit = api_offset_and_limit
+    # @festivals =  Festival.find :all,
+    #                     :limit  =>  @limit,
+    #                     :offset =>  @offset
     respond_to do |format|
       format.html # index.html.erb
       format.json { render :json => {:Success => true, :Data => @festivals}, :callback => params[:callback] }
     end
   end
 
-  # GET /users/1
-  # GET /users/1.json
+  # GET /festivals/1
+  # GET /festivals/1.json
   def show
     @festival = Festival.find(params[:id])
 
@@ -25,8 +25,8 @@ class FestivalsController < ApplicationController
     end
   end
 
-  # GET /users/new
-  # GET /users/new.json
+  # GET /festivals/new
+  # GET /festivals/new.json
   def new
     @festival = Festival.new
 
@@ -36,19 +36,19 @@ class FestivalsController < ApplicationController
     end
   end
 
-  # GET /users/1/edit
+  # GET /festivals/1/edit
   def edit
     @festival = Festival.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.json
+  # POST /festivals
+  # POST /festivals.json
   def create
     @festival = Festival.new(params[:festival])
 
     respond_to do |format|
       if @festival.save
-        format.html { redirect_to @festival, notice: 'User was successfully created.' }
+        format.html { redirect_to @festival, notice: 'Festival was successfully created.' }
         format.json { render json: @festival, status: :created, location: @festival }
       else
         format.html { render action: "new" }
@@ -57,14 +57,14 @@ class FestivalsController < ApplicationController
     end
   end
 
-  # PUT /users/1
-  # PUT /users/1.json
+  # PUT /festivals/1
+  # PUT /festivals/1.json
   def update
     @festival = Festival.find(params[:id])
 
     respond_to do |format|
       if @festival.update_attributes(params[:festival])
-        format.html { redirect_to @festival, notice: 'User was successfully updated.' }
+        format.html { redirect_to @festival, notice: 'Festival was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -73,11 +73,11 @@ class FestivalsController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
-  # http://localhost:3000/users/destroy/1
-  # http://wgo-hung-ror.herokuapp.com/users/destroy/1
-  # This URL is used to delete or destroy a particular user
+  # DELETE /festivals/1
+  # DELETE /festivals/1.json
+  # http://localhost:3000/festivals/destroy/1
+  # http://wgo-hung-ror.herokuapp.com/festivals/destroy/1
+  # This URL is used to delete or destroy a particular festival
   def destroy
     @festival = Festival.find(params[:id])
     @festival.destroy
